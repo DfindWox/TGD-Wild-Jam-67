3e4d07c02ea25c2d88a6416168612be886a48827
Natan Maia <natanmaiaplays@outlook.com>
2024-03-13 13:11:00 -0300

    objetos exemplo

---
+++ Objects/breakable_object.gd
@@ -0,0 +1,22 @@
+class_name BreakableObject
+extends CharacterBody2D
+## Pode ser usado pra diversos tipos de destrutiveis
+## seja impacto, fogo, etc etc.
+## Use collision layers diferentes pra tipos diferentes de ataques.
+
+@export var break_area : Area2D
+var animation_player : AnimationPlayer
+
+func _ready():
+	break_area.area_entered.connect(_on_area_entered_breakarea)
+	animation_player = $AnimationPlayer
+
+## A animação "break" pode ser customizada pra cada caso.
+func break_object():
+	break_area.process_mode = Node.PROCESS_MODE_DISABLED
+	animation_player.play("break")
+	await animation_player.animation_finished
+	queue_free()
+
+func _on_area_entered_breakarea(area:Area2D):
+	break_object()
