7705041b2219677d073acf36c94a70f3b328c7e5
Davi Sampaio <Davisampaio033@gmail.com>
2024-03-14 10:17:08 -0300

    skills added

--- Actors/parasite.gd
+++ Actors/parasite.gd
@@ -2,4 +2,16 @@ extends Player
 
 func skills():
 	if Input.is_action_just_pressed("ui_accept"):
-		pass
+		take_over()
+
+func take_over():
+	print_debug("taking over")
+	$AnimationPlayer.play("attack")
+	pass
+
+func _on_area_2d_body_entered(body):
+	#if attack_area_has_enemy:
+		#$AnimationPlayer.play("connect")
+		#has_parasite = false
+		#body.has_parasite = true
+	pass
