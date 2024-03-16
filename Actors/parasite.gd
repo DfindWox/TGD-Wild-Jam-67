520a50322ddf0e3490586d2ad0e85200fdad857b
Natan Maia <natanmaiaplays@outlook.com>
2024-03-15 22:01:48 -0300

    mecanica de possuir bichos

--- Actors/parasite.gd
+++ Actors/parasite.gd
@@ -1,20 +1,69 @@
+class_name Parasite
 extends Player
+## usar o class name só para checar se essa cena é parasita ou não
+
+var is_taking_over := false
+var is_facing_right := false
+
+var takeover_detection_count : int = 0
 
 func _ready():
 	has_parasite = true
 
-func player_skills(_delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		take_over()
+func _process(_delta):
+	if velocity.x > 0:
+		is_facing_right = true
+	elif velocity.x < 0:
+		is_facing_right = false
+	
+	# show popup
+	%AttackArea.scale.x = 1 if is_facing_right else -1
+	%TakeOverDetectionArea.scale.x = 1 if is_facing_right else -1
+	if takeover_detection_count > 0:
+		%TakeOverLabel.visible = true
+		%TakeOverLabel.global_position = $AttackArea/AttackCollision.global_position
+		%TakeOverLabel.global_position += Vector2(0, -32) - %TakeOverLabel.pivot_offset
+	else:
+		%TakeOverLabel.visible = false
 
-func take_over():
+func _physics_process(delta):
+	if is_taking_over:
+		basic_physics(delta)
+		move_and_slide() # só move, nao aceita input
+	else:
+		super(delta) # chama a função da classe Player
+
+func player_skills(_delta):
+	if Input.is_action_just_pressed("take_over"):
+		attempt_take_over()
+
+func attempt_take_over():
 	print_debug("taking over")
 	$AnimationPlayer.play("attack")
 	pass
 
+func do_take_over(body:Player):
+	is_taking_over = true
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+	velocity.x = move_speed * (1 if is_facing_right else -1) * 0.5
+	velocity.y = -jump_velocity * 0.5
+	move_and_slide()
+	await get_tree().create_timer(0.2).timeout
+	if is_dead: return
+	body.has_parasite = true
+	queue_free()
+
+# Apenas objetos na layer 9: ENEMY podem entrar
 func _on_area_2d_body_entered(body):
-	#if attack_area_has_enemy:
-		#$AnimationPlayer.play("connect")
-		#has_parasite = false
-		#body.has_parasite = true
-	pass
+	if is_taking_over: return
+	if not body is Player: return
+	# if not, try to take over
+	do_take_over(body)
+
+# Apenas objetos na layer 9: ENEMY podem entrar
+func _on_take_over_detection_area_body_entered(body):
+	takeover_detection_count += 1
+
+func _on_take_over_detection_area_body_exited(body):
+	takeover_detection_count -= 1
--- Actors/player.gd
+++ Actors/player.gd
@@ -1,6 +1,8 @@
 class_name Player
 extends CharacterBody2D
 
+var parasite_scene_packed : PackedScene = load("res://Actors/parasite.tscn")
+
 @export var has_parasite:bool = false
 @export var max_jumps : int = 1
 @export var move_speed : float = 200.0
@@ -17,7 +19,6 @@ func ai_behavior(_delta:float):
 
 ## virtual, codigo pra usar outras habilidades pelo parasita (tiro, voo) 
 func player_skills(_delta:float):
-	#print_debug("RAAWWNNNN")
 	pass
 
 func player_move(_delta): # delta não é usado ainda pois ta incluso em move_and_slide
@@ -42,9 +43,23 @@ func _physics_process(delta):
 			player_skills(delta)
 		else:
 			ai_behavior(delta)
+		# parasita sair do bixo
+		if has_parasite and Input.is_action_just_pressed("take_over"):
+			detach_parasite()
 	else:
 		velocity.x = 0
 	move_and_slide()
+
+func detach_parasite():
+	has_parasite = false
+	var parasite = parasite_scene_packed.instantiate()
+	get_tree().current_scene.add_child(parasite)
+	parasite.global_position = global_position + Vector2(0, -16)
+	parasite.velocity.y = -100
+	parasite.velocity.x = 100 * [-1,1].pick_random()
+	parasite.is_taking_over = true
+	await get_tree().create_timer(0.5).timeout
+	parasite.is_taking_over = false
 
 func kill():
 	is_dead = true
