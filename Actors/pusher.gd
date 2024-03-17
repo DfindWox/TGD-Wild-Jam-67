9043f7147e229ed6dcf8a423abb8db67510e1863
Davi Sampaio <Davisampaio033@gmail.com>
2024-03-17 09:51:41 -0300

    pusher AI start

--- Actors/pusher.gd
+++ Actors/pusher.gd
@@ -1,1 +1,28 @@
 extends Player
+<<<<<<< Updated upstream
+=======
+
+var push_area_count : int = 0
+@export var move_speed_mod : float = 0.2
+func _process(delta):
+	# animations
+	%Sprite2D.flip_h = not is_facing_right
+	
+	if push_area_count > 0 and velocity.y != 0:
+		if %AnimationPlayer.current_animation in ["jump", ""]:
+			$AnimationPlayer.play("pushStart")
+
+func ai_behavior(_delta):
+	velocity.x = move_speed * move_speed_mod * -1
+	pass
+
+func _on_push_area_area_entered(_body):
+	if not is_dead:
+		push_area_count += 1
+		$AnimationPlayer.play("pushStart")
+
+func _on_push_area_area_exited(_body):
+	if not is_dead:
+		push_area_count -= 1
+		$AnimationPlayer.play("idle")
+>>>>>>> Stashed changes
--- Actors/shooter.gd
+++ Actors/shooter.gd
@@ -4,6 +4,22 @@ var bullet_packed : PackedScene = load("res://Actors/shooter_bullet.tscn")
 
 @onready var attack_timer : Timer = $AttackTimer
 
+<<<<<<< Updated upstream
+=======
+func _process(_delta):
+	# animations
+	%Sprite2D.flip_h = not is_facing_right
+	$RayCast2D.scale.x = 1 if is_facing_right else -1
+
+func ai_behavior(_delta):
+	if attack_timer.is_stopped():
+		var collided_body = $RayCast2D.get_collider()
+		if collided_body and collided_body is Player and collided_body.has_parasite and $AttackDelay.is_stopped():
+			$AttackDelay.start()
+		else: #attempt failed
+			$AnimationPlayer.play("land")
+			attack_timer.start()
+>>>>>>> Stashed changes
 
 func player_skills(_delta:float):
 	if Input.is_action_just_pressed("attack"):
