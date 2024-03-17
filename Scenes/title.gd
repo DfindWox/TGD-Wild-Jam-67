62fcafe0794e355511ea3fe7c6c5eb27bf12e76f
Natan Maia <natanmaiaplays@outlook.com>
2024-03-17 11:39:20 -0300

    menus e creditos com arte

--- Scenes/title.gd
+++ Scenes/title.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _ready():
 	# stuff before fade
+	await Fade.fade_out(0.001)
 	AudioManager.play_music("1-stargazing_0.mp3")
 	await Fade.fade_in(0.2)
 	AudioManager.install_ui(self)
@@ -17,7 +18,9 @@ func _on_btn_start_pressed():
 	AudioManager.stop_music()
 	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	AudioManager.play_music("2-garden_0.mp3")
 	await Fade.fade_in()
+	
 
 func _on_btn_credits_pressed():
 	await Fade.fade_out(0.2)
